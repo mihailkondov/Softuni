81d2c6d737904a93c34e52a4cfa327fb6f3e406e
mastersho <116218303+mastersho@users.noreply.github.com>
2024-12-10 17:17:42 +0200

    EF Core/01 exercise 5

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/05. Change Town Names Casing/05.Change_Town_Names_Casing/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using static Azure.Core.HttpHeader;
+
+namespace _05.Change_Town_Names_Casing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            // Establish connection
+            string connectionString = @"Server=.;Database=MinionsDB;TrustServerCertificate=True;Trusted_Connection=True;Connection Timeout=5";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            // User input
+            string country = Console.ReadLine();
+
+            // Execute query
+            SqlCommand sqlCommand = new SqlCommand(SqlCommands.TOWNS_UPPER, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@Country", country);
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+            int recordsAffected = reader.RecordsAffected;
+
+            // Print results
+            if (recordsAffected == 0 )
+            {
+                Console.WriteLine("No town names were affected.");
+            }
+            else
+            {
+                Console.WriteLine($"{reader.RecordsAffected} town names were affected.");
+                List<string> townNames = new List<string>();
+                while (reader.Read())
+                {
+                    townNames.Add(reader["Name"].ToString());
+                }
+                Console.Write("[");
+                Console.Write(string.Join(", ", townNames));
+                Console.WriteLine("]");
+            }
+        }
+    }
+}
---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/05. Change Town Names Casing/05.Change_Town_Names_Casing/SqlCommands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Change_Town_Names_Casing
+{
+    public static class SqlCommands
+    {
+        /// <summary>
+        /// Executing this sql command will change all town names for a given @Country to uppercase in the table Towns. 
+        /// Like this: New York -> NEW YORK.
+        /// Takes string @Country as parameter.
+        /// </summary>
+        internal static string TOWNS_UPPER = @"
+	        UPDATE 
+		        Towns
+	        SET 
+		        [Name] = UPPER([Name])
+	        WHERE 
+		        CountryCode IN (SELECT Id FROM Countries WHERE [Name] = @Country);
+	        ;
+
+            SELECT 
+		        t.[Name] 
+	        FROM 
+		        Towns AS t 
+		        JOIN Countries AS c ON t.CountryCode = c.Id 
+	        WHERE 
+		        c.Name = @Country
+	        ;
+        ";
+    }
+}
