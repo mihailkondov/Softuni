7e5d80744fbe440ef832defc8d3140b5dfcff6ea
mastersho <116218303+mastersho@users.noreply.github.com>
2024-12-10 18:43:25 +0200

    EF Core/01 exercise 6

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/06. Remove Villain/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace _06._Remove_Villain
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // User Input - villain ID
+            Console.WriteLine("Please enter an integer - the ID of a villain to be deleted.");
+            int villainId = int.Parse(Console.ReadLine());
+
+            // Establish Connection
+            string connectionString = @"Server=.;Database=MinionsDB;TrustServerCertificate=True;Trusted_Connection=True;Connection Timeout=5";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            await sqlConnection.OpenAsync();
+
+            // Execute commands in database
+            SqlCommand cmd = new SqlCommand(SqlCommands.REMOVE_VILLAIN, sqlConnection);
+            cmd.Parameters.AddWithValue("@VillainId", villainId);
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            // Print result
+            bool found = await reader.ReadAsync();
+            if (found)
+            {
+                await Console.Out.WriteLineAsync($"{reader["Name"]} was deleted.");
+
+                reader.NextResult();
+                await reader.ReadAsync();
+                await Console.Out.WriteLineAsync($"{reader["RowsAffected"]} minions were released.");
+            }
+            else
+            {
+                await Console.Out.WriteLineAsync("No such villain was found.");
+            }
+        }
+    }
+}
---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/06. Remove Villain/SqlCommands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._Remove_Villain
+{
+    public static class SqlCommands
+    {
+        public static string REMOVE_VILLAIN = @"
+            SELECT 
+                [Name] 
+            FROM 
+                Villains 
+            WHERE 
+                Id = @VillainId
+            ;
+
+            DELETE 
+            FROM 
+                MinionsVillains
+	        WHERE 
+                VillainId = @VillainId
+            ;
+
+	        SELECT @@ROWCOUNT AS RowsAffected
+            ;
+
+	        DELETE 
+            FROM 
+                Villains
+	        WHERE 
+                Id = @VillainId
+	        ;
+        ";
+    }
+}
