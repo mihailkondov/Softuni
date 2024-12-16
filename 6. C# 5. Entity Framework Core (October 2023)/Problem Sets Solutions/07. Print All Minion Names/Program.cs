04523e7cd6d6fc42845c63c90387d1001b371bad
mastersho <116218303+mastersho@users.noreply.github.com>
2024-12-16 20:39:48 +0200

    EF Core/01 exercise 7

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/07. Print All Minion Names/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using static _07._Print_All_Minion_Names.SqlCommands;
+
+namespace _07._Print_All_Minion_Names
+{
+    /// <summary>
+    ///  The program prints all minion names from the minions table in the following order: first record, last 
+    /// record, first + 1, last - 1, first + 2, last - 2 … first + n, last - n.
+    /// For example the sequence of numbers from 1 to 15 becomes:
+    /// 1 15 2 14 3 13 4 12 5 11 6 10 7 9 8
+    /// </summary>
+    public class Program
+    {
+        const string _connectionString = @"
+            Connection Timeout=5;
+            Database=MinionsDB;
+            Integrated Security=True;
+            Server=COMPUTER;
+            TrustServerCertificate=True
+        ";
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(_connectionString);
+            sqlConnection.Open();
+
+            using SqlCommand getMinionNames = new SqlCommand(SqlCommands.selectAllMinions, sqlConnection);
+
+            using SqlDataReader minionNameReader = getMinionNames.ExecuteReader();
+
+            List<string> list = new List<string>();
+            while(minionNameReader.Read())
+            {
+                string name = minionNameReader.GetString(0);
+                list.Add(name);
+            }
+
+            Console.WriteLine("Initial order of minions:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Reordered minions:");
+            while (list.Count > 0)
+            {
+                Console.WriteLine(list[0]);
+                list.RemoveAt(0);
+                if(list.Count > 0) 
+                {
+                    Console.WriteLine(list[list.Count - 1]);
+                    list.RemoveAt(list.Count - 1);
+                    
+                }
+            }
+        }
+    }
+}
---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/07. Print All Minion Names/SqlCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._Print_All_Minion_Names
+{
+    public static class SqlCommands
+    {
+        public static string selectAllMinions = @"
+            SELECT [Name] FROM Minions;
+        ";
+    }
+}
