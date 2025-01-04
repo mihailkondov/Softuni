4ac6c86295281dd7258be90477f1812aefa6a075
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-04 04:26:04 +0200

    EF Core/02 Custom ORM - Change tracker ready

--- 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/02. Building a Custom ORM Framework/MiniORM/ChangeTracker.cs
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/02. Building a Custom ORM Framework/MiniORM/ChangeTracker.cs
@@ -1,7 +1,40 @@
-﻿namespace MiniORM
+﻿
+using System.Reflection;
+
+namespace MiniORM
 {
-    public class ChangeTracker
+    public class ChangeTracker<T> where T : class, new() 
+    // new() means that whatever class you specify for T, it has a default (no parameters) constructor.
+    // Therefore, in the generic class, you can do new T() and it will create a new object of type T.
     {
-        // TODO: Create your ChangeTracker class here.
+        private readonly List<T> _allEntities = new List<T>();
+        private readonly List<T> _added = new List<T>();
+        private readonly List<T> _removed = new List<T>();
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            _added = new List<T>();
+            _removed = new List<T>();
+            _allEntities = CloneEntities(entities);
+        }
+
+        private List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            List<T> clonedEntities = new List<T>();
+            var propertiesToClone = typeof(T).GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType))
+                .ToArray();
+            foreach (var entity in entities)
+            {
+                var clonedEntity = Activator.CreateInstance<T>();
+                foreach (PropertyInfo property in propertiesToClone)
+                {
+                    var value = property.GetValue(entity);
+                    property.SetValue(clonedEntity, value);
+                }
+                clonedEntities.Add(clonedEntity);
+            }
+            return clonedEntities;
+        }
     }
 }
--- 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/02. Building a Custom ORM Framework/MiniORM/DbContext.cs
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/02. Building a Custom ORM Framework/MiniORM/DbContext.cs
@@ -3,5 +3,16 @@
     public class DbContext
     {
         // TODO: Create your DbContext class here.
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string)
+        ,   typeof(int)
+        ,   typeof(uint)
+        ,   typeof(long)
+        ,   typeof(ulong)
+        ,   typeof(decimal)
+        ,   typeof(bool)
+        ,   typeof(DateTime)
+        };
     }
 }
