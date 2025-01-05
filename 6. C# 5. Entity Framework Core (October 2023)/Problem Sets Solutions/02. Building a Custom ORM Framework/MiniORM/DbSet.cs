f9b69309aec5da4dd8fd939a95d48b8d4a357bcf
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-05 20:21:22 +0200

    EF Core/02 Custom ORM - DbSet class implemented

--- 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/02. Building a Custom ORM Framework/MiniORM/DbSet.cs
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/02. Building a Custom ORM Framework/MiniORM/DbSet.cs
@@ -1,9 +1,87 @@
-﻿namespace MiniORM
+﻿using System.Collections;
+
+namespace MiniORM
 {
-    public class DbSet<TEntity> where TEntity : class, new()
+    public class DbSet<TEntity> : ICollection<TEntity>
+        where TEntity : class, new()
     {
-        // TODO: Create your DbSet class here.
+        // Constructor
+        internal DbSet(IEnumerable<TEntity> entities)
+        {
+            Entities = entities.ToList();
+            ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+
+        // Properties
+        public int Count => Entities.Count;
+
+        public bool IsReadOnly => Entities.IsReadOnly;
+
         internal ChangeTracker<TEntity> ChangeTracker { get; set; }
         internal IList<TEntity> Entities { get; set; }
+
+        // Methods
+        public void Add(TEntity entity)
+        {
+            if(entity == null)
+            {
+                throw new ArgumentNullException("Item cannot be null.");
+            }
+
+            Entities.Add(entity);
+            ChangeTracker.Add(entity);
+        }
+
+        public void Clear()
+        {
+            while (Entities.Any())
+            {
+                Entities.Remove(Entities.First());
+            }
+        }
+
+        public bool Contains(TEntity item)
+        {
+            return Entities.Contains(item);
+        }
+
+        public void CopyTo(TEntity[] array, int arrayIndex)
+        {
+            Entities.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return Entities.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool Remove(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Item cannot be null.");
+            }
+
+            bool removedSuccessfully = Entities.Remove(item);
+            if (removedSuccessfully)
+            {
+                ChangeTracker.Remove(item);
+            }
+
+            return removedSuccessfully;
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach(var entity in entities.ToArray())
+            {
+                Remove(entity);
+            }
+        }
     }
 }
