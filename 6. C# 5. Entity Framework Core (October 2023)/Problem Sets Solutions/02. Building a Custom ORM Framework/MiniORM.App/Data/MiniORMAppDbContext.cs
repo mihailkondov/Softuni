55938b6b98f228e56c4f2dcc02e622542d8f0235
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-07 20:33:38 +0200

    EF Core/02 Custom ORM - Demo app database creation script entities done

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/02. Building a Custom ORM Framework/MiniORM.App/Data/MiniORMAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using MiniORM.App.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App.Data
+{
+    public class MiniORMAppDbContext : DbContext
+    {
+        protected MiniORMAppDbContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+        public DbSet<Department> Departmens { get; }
+        public DbSet<Employee> Employees { get; }
+        public DbSet<EmployeeProject> EmployeesProject { get; }
+        public DbSet<Project> Projects { get; }
+    }
+}
