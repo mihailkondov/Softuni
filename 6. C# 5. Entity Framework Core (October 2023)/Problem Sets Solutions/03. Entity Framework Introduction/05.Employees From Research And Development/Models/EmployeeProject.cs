506f316d46893531f923a00537705fc379421cbc
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-11 17:14:05 +0200

    EF Core/03 Introduction - Exercise 05

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/03. Entity Framework Introduction/05.Employees From Research And Development/Models/EmployeeProject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni.Models
+{
+    public partial class EmployeeProject
+    {
+        //public EmployeeProject()
+        //{
+        //    Employee = new HashSet<Employee>();
+        //    Projects = new HashSet<Project>();
+        //}
+        public int EmployeeId { get; set; }
+        public int ProjectId { get; set; }
+        public virtual Project Project { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/03. Entity Framework Introduction/05.Employees From Research And Development/StartUp.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Extract all employees from the Research and Development department. 
+    Order them by salary (in ascending order), then by first name (in descending order). 
+    Return only their first name, last name, department name and salary rounded to 2 symbols
+*/
+
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new();
+            string info = GetEmployeesFromResearchAndDevelopment(context);
+            Console.WriteLine(info);
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Include(e => e.Department)
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new { e.FirstName, e.LastName, DepartmentName = e.Department.Name, e.Salary })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            string result = string.Join(
+                Environment.NewLine,
+                employees.Select(e => $"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:F2}")
+            );
+
+            return result;
+        }
+    }
+}
