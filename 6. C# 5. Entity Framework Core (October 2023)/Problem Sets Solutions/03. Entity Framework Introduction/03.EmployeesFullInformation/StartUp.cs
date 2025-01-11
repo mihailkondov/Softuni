a9c2e1dfeeb6a28c06ffcc7c468e34169d4a1b01
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-11 02:13:04 +0200

    EF Core/03 Introduction - Exercise 03

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/03. Entity Framework Introduction/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+namespace SoftUni
+{
+    /*
+        Your first task is to extract all employees and
+        return their first, last and middle name, their job title and salary, 
+        rounded to 2 symbols after the decimal separator,
+        all of those separated with a space. Order them by employee id.
+     */
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new();
+            string info = GetEmployeesFullInformation(context);
+            Console.WriteLine(info);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray();
+            var result = string.Join(Environment.NewLine, employees.Select(
+                e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
+
+
+
+            return result;
+        }
+    }
+}
