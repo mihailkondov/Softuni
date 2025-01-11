626132c25f3c35c65fd672e4935248de4c312d1b
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-11 18:15:01 +0200

    EF Core/03 Introduction - Exercise 06

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/03. Entity Framework Introduction/06.Adding A New Address And Updating Employee/StartUp.cs
@@ -0,0 +1,43 @@
+﻿/*
+    Create a new address with the text "Vitoshka 15" and TownId = 4. 
+    Set that address to the employee with last the name "Nakov".
+    Then order by descending all the employees by their Address' Id, take 10 rows and from them, take the AddressText. 
+    Return the results each on a new line.
+*/
+
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new();
+            string info = AddNewAddressToEmployee(context);
+            Console.WriteLine(info);
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            Employee? nakov = context.Employees
+                .Where(e => e.LastName == "Nakov")
+                .FirstOrDefault();
+            nakov.Address = new Address() { AddressText = "Vitoshka 15", TownId = 4 };
+            context.SaveChanges();
+
+            var addresses = context.Addresses
+                .OrderByDescending(a => a.AddressId)
+                .Take(10)
+                .Select(a => new { a.AddressText })
+                .ToArray();
+            string result = string.Join(
+                Environment.NewLine,
+                addresses.Select(e => $"{e.AddressText}")
+            );
+
+            return result;
+        }
+    }
+}
