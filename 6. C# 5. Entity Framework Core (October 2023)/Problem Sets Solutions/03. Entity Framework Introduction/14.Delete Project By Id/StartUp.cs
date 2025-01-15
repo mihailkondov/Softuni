1325f039f0e543b10825e97efc45c80506a87924
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-15 11:59:34 +0200

    EF Core/03 Introduction - Exercise 14 - Delete Project By Id

---
+++ 6. C# 5. Entity Framework Core (October 2023)/Problem Sets Solutions/03. Entity Framework Introduction/14.Delete Project By Id/StartUp.cs
@@ -0,0 +1,49 @@
+﻿/*
+    Let's delete the project with id 2. 
+    Then, take 10 projects and return their names, each on a new line. 
+    Remember to restore your database after this task.
+*/
+
+using SoftUni.Data;
+using System.Text;
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new();
+            string info = DeleteProjectById(context);
+            Console.WriteLine(info);
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            var projectsToRemove = context.EmployeesProjects.Where(ep => ep.ProjectId == 2);
+            foreach (var project in projectsToRemove)
+            {
+                context.EmployeesProjects.Remove(project);
+            }
+
+            var projectToDelete = context.Projects.FirstOrDefault(p => p.ProjectId == 2);
+            if(projectToDelete != null)
+            {
+                context.Remove(projectToDelete);
+            }
+
+            context.SaveChanges();
+
+            var projects = context.Projects
+                .Take(10)
+                .Select(p => p.Name)
+                .ToList();
+
+            StringBuilder sb = new();
+            foreach (var project in projects)
+            {
+                sb.AppendLine($"{project}");
+            }
+            return sb.ToString();
+        }
+    }
+}
