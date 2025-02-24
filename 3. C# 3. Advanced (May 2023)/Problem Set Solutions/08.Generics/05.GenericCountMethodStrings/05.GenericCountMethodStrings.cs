e9cd1126eec6856c6f3fdd0b2599acff97ed34d6
mastersho <116218303+mastersho@users.noreply.github.com>
2025-02-24 19:54:41 +0200

    C# Advanced/08 Generics - Ex. 5

---
+++ 3. C# 3. Advanced (May 2023)/Problem Set Solutions/08.Generics/05.GenericCountMethodStrings/05.GenericCountMethodStrings.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.GenericCountMethodStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            string element = Console.ReadLine();
+            int output = CountGreaterThan(list, element);
+            Console.WriteLine(output);
+        }
+
+        static int CountGreaterThan<T>(List<T> list, T element) where T : IComparable<T>
+        {
+            int count = 0;
+            foreach (T listEntry in list)
+            {
+                int diff = listEntry.CompareTo(element);
+                if ( diff > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
