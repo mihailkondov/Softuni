9d0ee3ddc355e70a55655acef8bd3087c7e6fb83
mastersho <116218303+mastersho@users.noreply.github.com>
2025-02-26 00:46:59 +0200

    C# Advanced/08 Generics - Problem Set Completed

---
+++ 3. C# 3. Advanced (May 2023)/Problem Set Solutions/08.Generics/09.CustomLinkedList/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CustomDoublyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomGenericListDemo();
+        }
+
+        static void CustomGenericListDemo()
+        {
+            var list = new CustomDoublyLinkedList<int>();
+            string className = list.GetType().Name.Split("`")[0];
+            Action<int> print = (int x) => Console.WriteLine($"    {x}");
+            // Demo:
+            Console.WriteLine($"Testing the {className} class:");
+            Console.WriteLine("  Initial Count is " + list.Count);
+
+            Console.WriteLine("  Populating list");
+
+            list.AddFirst(2);
+            list.AddFirst(1);
+            list.AddLast(3);
+            list.AddLast(4);
+            list.ForEach(print);
+            Console.WriteLine($"  Count is {list.Count}");
+            Console.WriteLine();
+
+            Console.WriteLine("  Removing first element.");
+            list.RemoveFirst();
+            Console.WriteLine("  Removing last element:");
+            list.RemoveLast();
+            list.ForEach(print);
+            Console.WriteLine("    Count is " + list.Count);
+
+            Console.WriteLine("  Removing first element.");
+            list.RemoveFirst();
+            Console.WriteLine("  Removing last element:");
+            list.RemoveLast();
+            list.ForEach(print);
+
+            list.AddFirst(1);
+            list.RemoveFirst();
+
+            list.ForEach(print);
+            Console.WriteLine("    Count is " + list.Count);
+        }
+    }
+}
