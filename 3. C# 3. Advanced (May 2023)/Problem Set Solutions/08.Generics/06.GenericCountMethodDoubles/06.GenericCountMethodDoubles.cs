edde4bc5badfe07d3460da9eb950757852912cfb
mastersho <116218303+mastersho@users.noreply.github.com>
2025-02-25 21:58:05 +0200

    C# Advanced/08 Generics - Ex. 6

---
+++ 3. C# 3. Advanced (May 2023)/Problem Set Solutions/08.Generics/06.GenericCountMethodDoubles/06.GenericCountMethodDoubles.cs
@@ -0,0 +1,32 @@
+﻿namespace _06.GenericCountMethodDoubles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> list = new List<double>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(double.Parse(Console.ReadLine()));
+            }
+            double element = double.Parse(Console.ReadLine());
+            int output = CountGreaterThan(list, element);
+            Console.WriteLine(output);
+        }
+
+        static int CountGreaterThan<T>(List<T> list, T element) where T : IComparable<T>
+        {
+            int count = 0;
+            foreach (T listEntry in list)
+            {
+                int diff = listEntry.CompareTo(element);
+                if (diff > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
