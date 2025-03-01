b92181ec6d576c6f91b9b1e66b4eda1b7f8148c0
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-01 20:20:55 +0200

    C# Advanced/09 Iterators And Comparators - Problem Set Completed

---
+++ 3. C# 3. Advanced (May 2023)/Problem Set Solutions/09.IteratorsAndComparators/08.CustomLinkedList/CustomDoublyLinkedList.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+
+namespace CustomDoublyLinkedList
+{
+    public class CustomDoublyLinkedList<T> : IEnumerable<T>
+    {
+        private class ListNode
+        {
+            public T Value { get; set; }
+            public ListNode? NextNode { get; set; }
+            public ListNode? PreviousNode { get; set; }
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+        }
+
+        // Fields
+        private ListNode? head;
+        private ListNode? tail;
+
+        // Constructor
+        public CustomDoublyLinkedList()
+        {
+            Count = 0;
+            head = null;
+            tail = null;
+        }
+
+        // Properties
+        public int Count { get; private set; }
+
+        // Public methods
+        public void AddFirst(T element)
+        {
+            if (Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode node = new ListNode(element);
+                head!.PreviousNode = node;
+                node.NextNode = head;
+                this.head = node;
+            }
+            Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            if (Count == 0)
+            {
+                this.tail = this.head = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(element);
+                newTail.PreviousNode = this.tail;
+                this.tail!.NextNode = newTail;
+                this.tail = newTail;
+            }
+            Count++;
+        }
+        public T RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("No elements to remove from an empty list.");
+            }
+
+            T returnValue = this.head.Value;
+            if (Count == 1)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else
+            {
+                this.head = head!.NextNode;
+                head!.PreviousNode = null;
+            }
+
+            Count--;
+            return returnValue;
+        }
+        public T RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("No elements to remove from an empty list.");
+            }
+
+            T returnValue = this.tail!.Value;
+            if (Count == 1)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else
+            {
+                this.tail = this.tail!.PreviousNode;
+                tail!.NextNode = null;
+            }
+
+            Count--;
+            return returnValue;
+        }
+        public void ForEach(Action<T> action)
+        {
+            if (Count == 0)
+            {
+                return;
+            }
+
+            ListNode? node = head;
+            while (node != null)
+            {
+                action(node.Value);
+                node = node.NextNode;
+            }
+        }
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+            ListNode? node = head;
+            int index = 0;
+            while (node != null)
+            {
+                array[index++] = node.Value;
+                node = node.NextNode;
+            }
+
+            return array;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            ListNode current = head;
+            while(current != null)
+            {
+                yield return current.Value;
+                current = current.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 3. C# 3. Advanced (May 2023)/Problem Set Solutions/09.IteratorsAndComparators/08.CustomLinkedList/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace CustomDoublyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomGenericListDemo();
+        }
+
+        static void CustomGenericListDemo()
+        {
+            //// Demo
+            var list = new CustomDoublyLinkedList<int>();
+            string className = list.GetType().Name.Split("`")[0];
+            Action<int> print = (int x) => Console.WriteLine($"    {x}");
+            Console.WriteLine($"Testing the {className} class:");
+
+            // Count
+            Console.WriteLine("  Initial Count is " + list.Count);
+
+            // AddFirst, AddLast methods
+            Console.WriteLine("  Populating list");
+
+            list.AddFirst(2);
+            list.AddFirst(1);
+            list.AddLast(3);
+            list.AddLast(4);
+            list.ForEach(print);
+            Console.WriteLine($"  Count is {list.Count}");
+            Console.WriteLine();
+
+            // RemoveFirst, RemoveLast methods
+            Console.WriteLine("  Removing first element.");
+            list.RemoveFirst();
+            Console.WriteLine("  Removing last element:");
+            list.RemoveLast();
+            list.ForEach(print);
+            Console.WriteLine("    Count is " + list.Count);
+            Console.WriteLine("  Removing first element.");
+            list.RemoveFirst();
+            Console.WriteLine("  Removing last element:");
+            list.RemoveLast();
+            list.ForEach(print);
+
+            list.AddFirst(1);
+            list.RemoveFirst();
+
+            // ForEach method
+            list.ForEach(print);
+            Console.WriteLine("    Count is " + list.Count);
+
+            // Enumerator
+            Console.WriteLine("  Testing enumerator:");
+            Console.WriteLine($"    On an empty list: {string.Join(" ", list)}");
+            list.AddLast(123);
+            Console.WriteLine($"    On a list with one item: {string.Join(" ", list)}");
+            list.AddLast(1234);
+            Console.WriteLine($"    On a list with two items: {string.Join(" ", list)}");
+            list.AddLast(12345);
+            list.AddFirst(12);
+            list.AddFirst(1);
+            Console.WriteLine($"    On a list with {list.Count} items: {string.Join(" ", list)}");
+
+            Console.WriteLine("    Trying a +1 transformation:");
+            Console.Write("    ");
+            foreach (var item in list)
+            {
+                Console.Write(item + 1 + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
