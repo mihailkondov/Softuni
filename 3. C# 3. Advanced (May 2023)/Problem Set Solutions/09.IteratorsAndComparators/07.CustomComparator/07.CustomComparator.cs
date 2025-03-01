692372dabb42075768eeaa358912ade403bc4f95
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-01 20:00:14 +0200

    C# Advanced/09 Iterators And Comparators - Ex. 7

---
+++ 3. C# 3. Advanced (May 2023)/Problem Set Solutions/09.IteratorsAndComparators/07.CustomComparator/07.CustomComparator.cs
@@ -0,0 +1,25 @@
+﻿/* 
+    7. Custom Comparator
+
+Write a custom comparator that sorts all even numbers before all 
+the odd ones in ascending order. Pass it to Array.Sort() 
+function and print the result. Use functions.
+ */
+
+Comparison<int> comparison = (x, y) =>
+{
+    bool xIsEven = x % 2 == 0;
+    bool yIsEven = y % 2 == 0;
+
+    if (xIsEven && (!yIsEven))
+        return -1;
+
+    if (!xIsEven && yIsEven)
+        return 1;
+
+    return x - y;
+};
+
+int[] ints = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+Array.Sort(array: ints, comparison: comparison);
+Console.WriteLine(string.Join(" ", ints));
