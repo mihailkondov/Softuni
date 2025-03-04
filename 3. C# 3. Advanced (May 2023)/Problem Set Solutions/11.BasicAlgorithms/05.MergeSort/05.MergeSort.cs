4c83156a03f921be0162156478191eb8f4ca9ec1
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-04 19:43:26 +0200

    C# Advanced/11 Basic Algorithms - Ex 5

---
+++ 3. C# 3. Advanced (May 2023)/Problem Set Solutions/11.BasicAlgorithms/05.MergeSort/05.MergeSort.cs
@@ -0,0 +1,52 @@
+﻿// 05. Merge Sort
+
+int[] ints = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+MergeSort<int>.Sort(ints);
+Console.WriteLine(string.Join(" ", ints));
+
+internal class MergeSort<T> where T : IComparable<T>
+{
+    public static void Sort(T[] array)
+    {
+        if (array.Length <= 1)
+            return;
+
+        if (array.Length == 2 && array[0].CompareTo(array[1]) > 0)
+        {
+            T tmp = array[0];
+            array[0] = array[1];
+            array[1] = tmp;
+            return;
+        }
+
+        int leftEndIndex = array.Length / 2 - 1;
+        int rightStartIndex = array.Length / 2;
+
+        // Sort subarrays
+        T[] left = array[..(leftEndIndex + 1)];
+        T[] right = array[(rightStartIndex)..];
+        Sort(left);
+        Sort(right);
+
+        // Merge subarrays
+        for (int i = 0, j = 0; i + j < array.Length;)
+        {
+            if (i + 1 > left.Length)
+            {
+                array[i + j] = right[j++];
+            }
+            else if (j + 1 > right.Length)
+            {
+                array[i + j] = left[i++];
+            }
+            else if (left[i].CompareTo(right[j]) < 0)
+            {
+                array[i + j] = left[i++];
+            }
+            else
+            {
+                array[i + j] = right[j++];
+            }
+        }
+    }
+}
