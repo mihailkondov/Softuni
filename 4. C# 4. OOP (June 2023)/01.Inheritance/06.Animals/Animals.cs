22c1a624b158eb8875ca6673e7aca734d7d7b5df
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-08 00:05:35 +0200

    C# OOP/01 Inheritance - Ex 6 - Animals

---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/06.Animals/Animals.cs
@@ -0,0 +1,126 @@
+﻿// 06. Animals - Classes
+
+using System.Text;
+
+namespace Animals
+{
+    public interface IProduceSound
+    {
+        public abstract string ProduceSound();
+    }
+
+    // Level 0 (parent classes)
+    public abstract class Animal : IProduceSound
+    {
+        public const string male = "Male";
+        public const string female = "Female";
+        private int _age;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                _age = value;
+            }
+        }
+        public string Gender { get; set; }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.GetType().Name);
+            sb.AppendLine($"{Name} {Age} {Gender}");
+            sb.Append(this.ProduceSound());
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    // Level 1 Child classes
+    public class Cat : Animal
+    {
+        public Cat(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow meow";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Woof!";
+        }
+    }
+
+    public class Frog : Animal
+    {
+        public Frog(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Ribbit";
+        }
+    }
+
+    // Level 2 child classes
+    public class Kitten : Cat
+    {
+        public Kitten(string name, int age)
+            : base(name, age, female)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+
+    public class Tomcat : Cat
+    {
+        public Tomcat(string name, int age)
+            : base(name, age, male)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "MEOW";
+        }
+    }
+}
+
---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/06.Animals/StartUp.cs
@@ -0,0 +1,54 @@
+﻿// 06. Animals
+
+namespace Animals;
+public class StartUp
+{
+    public static void Main()
+    {
+
+        string animalType = string.Empty;
+        Animal animal = null;
+        List<Animal> animals = new List<Animal>();
+        while ((animalType = Console.ReadLine()!) != "Beast!")
+        {
+            try
+            {
+                string[] description = Console.ReadLine()!
+                    .Split(' ')
+                    .ToArray();
+                string name = description[0];
+                int age = int.Parse(description[1]);
+                switch (animalType)
+                {
+                    case "Cat":
+                        animal = new Cat(name, age, description[2]);
+                        break;
+                    case "Dog":
+                        animal = new Dog(name, age, description[2]);
+                        break;
+                    case "Frog":
+                        animal = new Frog(name, age, description[2]);
+                        break;
+                    case "Kitten":
+                        animal = new Kitten(name, age);
+                        break;
+                    case "Tomcat":
+                        animal = new Tomcat(name, age);
+                        break;
+                    default:
+                        throw new Exception(message: "Invalid input!");
+                }
+                animals.Add(animal);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        foreach (var currentAnimal in animals)
+        {
+            Console.WriteLine(currentAnimal.ToString());
+        }
+    }
+}
