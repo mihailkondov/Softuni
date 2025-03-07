ff648d2f4817e40691fd1533b3028cf3f212033e
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-07 19:26:30 +0200

    C# OOP/01 Inheritance - Ex 5 - Restaurant

---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/05.Restaurant/Classes.cs
@@ -0,0 +1,141 @@
+﻿// 01. Person - Classes
+
+namespace Restaurant
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+
+    public class Beverage : Product
+    {
+        public Beverage(string name, decimal price, double millimeters)
+            : base(name, price)
+        {
+            this.Millimeters = millimeters;
+        }
+
+        public double Millimeters { get; set; }
+    }
+
+    public class HotBeverage : Beverage
+    {
+        public HotBeverage(string name, decimal price, double millimeters)
+            : base(name, price, millimeters)
+        {
+
+        }
+    }
+
+    public class Tea : HotBeverage
+    {
+        public Tea(string name, decimal price, double millimeters)
+            : base(name, price, millimeters)
+        {
+
+        }
+    }
+
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMillimeters = 50;
+        private const decimal CoffeePrice = 3.5m;
+
+        public Coffee(string name, double caffeine)
+            : base(name, CoffeePrice, CoffeeMillimeters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+
+    public class ColdBeverage : Beverage
+    {
+        public ColdBeverage(string name, decimal price, double millimeters)
+            : base(name, price, millimeters)
+        {
+
+        }
+    }
+
+    public class Food : Product
+    {
+        public Food(string name, decimal price, double grams)
+            : base(name, price)
+        {
+            this.Grams = grams;
+        }
+
+        public double Grams { get; set; }
+    }
+
+    public class MainDish : Food
+    {
+        public MainDish(string name, decimal price, double grams)
+            : base(name, price, grams)
+        {
+
+        }
+    }
+
+    public class Fish : MainDish
+    {
+        private const double FishGrams = 22;
+
+        public Fish(string name, decimal price)
+            : base(name, price, FishGrams)
+        {
+
+        }
+    }
+
+    public class Dessert : Food
+    {
+        public Dessert(string name, decimal price, double grams, double calories)
+            : base(name, price, grams)
+        {
+            this.Calories = calories;
+        }
+
+        public double Calories { get; set; }
+    }
+
+    public class Cake : Dessert
+    {
+        private const double CakeGrams = 250;
+        private const double CakeCalories = 1000;
+        private const decimal CakePrice = 5m;
+
+        public Cake(string name)
+            : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+
+        }
+    }
+
+    public class Starter : Food
+    {
+        public Starter(string name, decimal price, double grams)
+            : base(name, price, grams)
+        {
+
+        }
+    }
+
+    public class Soup : Starter
+    {
+        public Soup(string name, decimal price, double grams)
+            : base(name, price, grams)
+        {
+
+        }
+    }
+}
---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/05.Restaurant/Program.cs
@@ -0,0 +1,20 @@
+﻿// 05. Restaurant
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# OOP - Inheritance");
+            Console.WriteLine("    Exercise 05: Restaurant");
+            
+            Console.WriteLine();
+            Coffee coffee = new Coffee("Cappuccino", 6.0);
+            Cake cake = new Cake("Garage");
+
+            Console.WriteLine($"{coffee.Name}: {coffee.Price} lv, {coffee.Caffeine} caffeine, {coffee.Millimeters} quantity");
+            Console.WriteLine($"{cake.Name}: {cake.Price} lv, {cake.Grams} grams, {cake.Calories} calories");
+        }
+    }
+}
