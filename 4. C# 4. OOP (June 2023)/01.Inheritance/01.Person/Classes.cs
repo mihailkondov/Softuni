488afd3bc131559561568c2b1d64e18333273d9e
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-07 18:50:58 +0200

    C# OOP/01 Inheritance - Ex 1 - Person

---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/01.Person/Classes.cs
@@ -0,0 +1,45 @@
+﻿// 01. Person - Classes
+
+using System.Xml.Linq;
+
+namespace _01.Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int Age { get => age; set => age = value; }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        override public string ToString()
+        {
+            return $"Person -> Name: {Name}, Age: {Age}";
+        }
+    }
+
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+                
+        }
+
+        public override string ToString()
+        {
+            return $"Child -> Name: {base.Name}, Age: {base.Age}";
+        }
+    }
+}
---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/01.Person/StartUp.cs
@@ -0,0 +1,27 @@
+﻿// 01. Person - Startup
+
+namespace _01.Person
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            if (age < 0)
+            {
+                Console.WriteLine("Age must be positive!");
+            }
+            else if (age > 15)
+            {
+                Person person = new Person(name, age);
+                Console.WriteLine(person);
+            }
+            else
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+        }
+    }
+}
