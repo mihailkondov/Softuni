204422c64b76657961588c9edba243d44d4170d3
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-07 19:05:31 +0200

    C# OOP/01 Inheritance - Ex 3 - Players And Monsters

---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/03.PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,12 @@
+﻿// 03. Players And Monsters
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# OOP - Inheritance - Exercise 03: Players And Monsters.");
+        }
+    }
+}
