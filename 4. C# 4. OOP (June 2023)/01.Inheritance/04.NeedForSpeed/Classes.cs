b6270ae08f29255f11cfa4d8f92f2d1d0025806c
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-07 19:13:57 +0200

    C# OOP/01 Inheritance - Ex 4 - Need For Speed

---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/04.NeedForSpeed/Classes.cs
@@ -0,0 +1,98 @@
+﻿// 04. Need For Speed - Classes
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private double fuel;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+        public double Fuel
+        {
+            get { return this.fuel; }
+            set { this.fuel = value; }
+        }
+        public double DefaultFuelConsumption;
+        public virtual double FuelConsumption { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            fuel -= kilometers * 1.25;
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+    }
+
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+
+        public override void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * 8;
+        }
+    }
+
+    public class CrossMotorcycle : Motorcycle
+    {
+        public CrossMotorcycle(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+
+        public override void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * 3;
+        }
+    }
+
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+    }
+
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+
+        public override void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * 10;
+        }
+    }
+}
---
+++ 4. C# 4. OOP (June 2023)/01.Inheritance/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,13 @@
+﻿// 04 Need For Speed
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# OOP - Inheritance");
+            Console.WriteLine("    Exercise 04: Need For Speed");
+        }
+    }
+}
