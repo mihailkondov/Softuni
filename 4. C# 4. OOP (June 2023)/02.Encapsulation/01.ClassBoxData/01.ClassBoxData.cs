951e19bebb8cd346b1adfe2539096312af48c686
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-08 20:32:04 +0200

    C# OOP/02 Encapsulation - Ex 1 - Class Box Data

---
+++ 4. C# 4. OOP (June 2023)/02.Encapsulation/01.ClassBoxData/01.ClassBoxData.cs
@@ -0,0 +1,20 @@
+﻿// 01. Class Box Data - StartUp
+
+using _01.ClassBoxData;
+
+double[] dim = new double[3];
+dim[0] = double.Parse(Console.ReadLine()!);
+dim[1] = double.Parse(Console.ReadLine()!);
+dim[2] = double.Parse(Console.ReadLine()!);
+
+try
+{
+    Box box = new Box(length: dim[0], width: dim[1], height: dim[2]);
+    Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+    Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+    Console.WriteLine($"Volume - {box.Volume():F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ 4. C# 4. OOP (June 2023)/02.Encapsulation/01.ClassBoxData/Box.cs
@@ -0,0 +1,71 @@
+﻿// 01. Class Box Data - Box
+
+namespace _01.ClassBoxData
+{
+    internal class Box
+    {
+        double _length;
+        double _width;
+        double _height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length 
+        {
+            get => _length;
+            private set
+            {
+                if (value <= 0 )
+                {
+                    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                }
+                _length = value;
+            }
+        }
+        public double Width
+        {
+            get => _width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                }
+                _width = value;
+            }
+        }
+        public double Height
+        {
+            get => _height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                }
+                _height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return (Length * Height) * 2 + (Width * Length) * 2 + (Width * Height) * 2;
+
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return (Height * Width) * 2 + (Height * Length) * 2;
+        }
+
+        public double Volume()
+        {
+            return Height * Width * Length;
+        }
+    }
+}
