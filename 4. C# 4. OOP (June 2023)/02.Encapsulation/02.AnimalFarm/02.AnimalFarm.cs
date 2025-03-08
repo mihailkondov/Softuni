479f52c31f5ea7191c2bf0a025da670ca22d6665
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-08 20:43:07 +0200

    C# OOP/02 Encapsulation - Ex 2 - Animal Farm

---
+++ 4. C# 4. OOP (June 2023)/02.Encapsulation/02.AnimalFarm/02.AnimalFarm.cs
@@ -0,0 +1,34 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+            }
+
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Name cannot be empty.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Age should be between 0 and 15.");
+            }
+
+
+        }
+    }
+}
---
+++ 4. C# 4. OOP (June 2023)/02.Encapsulation/02.AnimalFarm/Models/Chicken.cs
@@ -0,0 +1,75 @@
+﻿namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        protected string name;
+        internal int age;
+
+        internal Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        internal string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new System.ArgumentNullException("Name cannot be empty.");
+
+                }
+                this.name = value;
+            }
+        }
+
+        internal int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private protected set
+            {
+                if (value < MinAge || value > MaxAge)
+                {
+                    throw new System.ArgumentOutOfRangeException("Age should be between 0 and 15.");
+                }
+                this.age = value;
+            }
+        }
+
+        internal double ProductPerDay
+        {
+            get
+            {
+                return this.CalculateProductPerDay();
+            }
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case <= 3:
+                    return 1.5;
+                case <= 7:
+                    return 2;
+                case <=11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
