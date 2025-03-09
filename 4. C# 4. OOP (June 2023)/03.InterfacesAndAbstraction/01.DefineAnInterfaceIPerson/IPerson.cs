1dae258db648114fcaee68b7b8f543035d01a65e
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-09 20:09:25 +0200

    C# OOP/02 Interfaces And Abstraction - Ex 1 - Define An Interface IPerson

---
+++ 4. C# 4. OOP (June 2023)/03.InterfacesAndAbstraction/01.DefineAnInterfaceIPerson/IPerson.cs
@@ -0,0 +1,10 @@
+﻿// 01. Define An Interface IPerson - Interface IPerson
+
+namespace PersonInfo
+{
+    public interface IPerson
+    {
+        string Name { get; }
+        int Age { get; }
+    }
+}
