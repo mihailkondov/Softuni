ddc641f4ce0705c7230c7656abde3408bf1cfffd
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-17 23:37:31 +0200

    C# OOP/05 Exception Handling - Ex 2 - Enter Numbers

---
+++ 4. C# 4. OOP (June 2023)/05.ExceptionHandling/02.EnterNumbers/02.EnterNumbers.cs
@@ -0,0 +1,37 @@
+﻿// 02. Enter Numbers
+
+List<int> ints = new List<int>();
+int min = 1;
+do
+{
+    try
+    {
+        if (ints.Count > 0)
+        {
+            min = ints[ints.Count - 1];
+        }
+        int newNumber = ReadNumber(min, 100);
+        ints.Add(newNumber);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid Number!");
+    }
+    catch (ArgumentException ae)
+    {
+        Console.WriteLine(ae.Message);
+    }
+} while (ints.Count < 10);
+
+Console.WriteLine(string.Join(", ", ints));
+
+static int ReadNumber(int start, int end)
+{
+    int n;
+    n = int.Parse(Console.ReadLine());
+    if (n <= start || n >= end)
+    {
+        throw new ArgumentException($"Your number is not in range {start} - 100!");
+    }
+    return n;
+}
