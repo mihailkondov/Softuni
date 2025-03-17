c716ccd5e1e600ef71f3866378e583fa1d488a2c
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-17 23:14:15 +0200

    C# OOP/05 Exception Handling - Ex 1 - Square Root

---
+++ 4. C# 4. OOP (June 2023)/05.ExceptionHandling/01.SquareRoot/01.SquareRoot.cs
@@ -0,0 +1,20 @@
+﻿// 01. Square Root
+
+double.TryParse(Console.ReadLine(), out double x);
+try
+{
+    double sqrtX = Math.Sqrt(x);
+    if (double.IsNaN(sqrtX))
+    {
+        throw new ArgumentException("Invalid number.");
+    }
+    Console.WriteLine(sqrtX);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
