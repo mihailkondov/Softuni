e805c267c786ca62b16c00883eaaa7202ecbe770
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-19 00:39:25 +0200

    C# OOP/05 Exception Handling - Ex 4 - Sum Of Integers

---
+++ 4. C# 4. OOP (June 2023)/05.ExceptionHandling/04.SumOfIntegers/04.SumOfIntegers.cs
@@ -0,0 +1,25 @@
+﻿// 04. Sum Of Integers
+
+string[] tokens = Console.ReadLine()!.Split(' ');
+int sum = 0;
+foreach (var token in tokens)
+{
+    try
+    {
+        int a = int.Parse(token);
+        sum += a;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{token}' is in wrong format!");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{token}' is out of range!");
+    }
+    finally
+    {
+        Console.WriteLine($"Element '{token}' processed - current sum: {sum}");
+    }
+}
+Console.WriteLine($"The total sum of all integers is: {sum}");
