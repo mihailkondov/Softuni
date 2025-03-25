0e07d67f8a600a9999cc158eb0654710bdf0b1e1
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-25 19:34:22 +0200

    C# OOP/07 SOLID - 01.Logger - Example Project

---
+++ 4. C# 4. OOP (June 2023)/07.SOLID/00.LoggerApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using _99.LoggerLibrary;
+using _99.LoggerLibrary.Appenders;
+using _99.LoggerLibrary.Enums;
+using _99.LoggerLibrary.Layouts;
+using _99.LoggerLibrary.Loggers;
+
+class Program
+{
+    static void Main()
+    {
+        var simpleLayout = new SimpleLayout();
+        var consoleAppender = new ConsoleAppender(simpleLayout);
+        consoleAppender.ReportLevel = ReportLevel.Error;
+        var logger = new Logger(consoleAppender);
+        logger.Info("3/31/2015 5:33:07 PM", "Everything seems fine");
+        logger.Warning("3/31/2015 5:33:07 PM", "Warning: ping is too high - disconnect imminent");
+        logger.Error("3/31/2015 5:33:07 PM", "Error parsing request");
+        logger.Critical("3/31/2015 5:33:07 PM", "No connection string found in App.config");
+        logger.Fatal("3/31/2015 5:33:07 PM", "mscorlib.dll does not respond");
+    }
+}
