f7534f09df77e9d6b854521d2cfb69081d720afe
mastersho <116218303+mastersho@users.noreply.github.com>
2025-03-25 19:32:58 +0200

    C# OOP/07 SOLID - 01.Logger - Core Project

---
+++ 4. C# 4. OOP (June 2023)/07.SOLID/00.LoggerApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 01. Logger 
+// This is a demonstration.
+
+using _99.LoggerLibrary.Appenders;
+using _99.LoggerLibrary.Loggers;
+using _99.LoggerLibrary.Loggers.Interface;
+using _99.LoggerLibrary.Appenders.Interface;
+using _99.LoggerLibrary.Layouts.Interface;
+using _99.LoggerLibrary.Layouts;
+
+ILayout simpleLayout = new SimpleLayout();
+IAppender consoleAppender = new ConsoleAppender(simpleLayout);
+ILogger logger = new Logger(consoleAppender);
+logger.Error("3/26/2015 2:08:11 PM", "Errorparsing JSON.");
+logger.Info("3/26/2015 2:08:11 PM", "User Pesho successfully registered.");
+logger.Warning("M/d/yyyy h:mm:ss tt", "Is my datetime format wrong?");
